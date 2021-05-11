f631fb863b8845c9f939c51f5f3e143d3bfdebfd
Daya140 <metamorfosedaya@gmail.com>
2021-05-11 20:21:54 -0300

    aula07

---
+++ Exercício1.py
@@ -0,0 +1,9 @@
+def soma(n1=0, n2=0, n3=0):
+    soma = n1 + n2 + n3
+    print(f'A soma é {soma}')
+
+soma(3,6,9)
+    
+
+
+
---
+++ aula06.py
@@ -0,0 +1,8 @@
+def soma(n1=0, n2=0, n3=0, n4=0):
+    soma = n1 + n2 + n3 + n4
+    print(f'A soma é {soma}')
+
+soma(10, 30)
+soma(4,7,5)
+soma(3,2,6,8)
+
